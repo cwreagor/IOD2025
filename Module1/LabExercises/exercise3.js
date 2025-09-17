b05d35f7239073a96d9468879b672e367964007f
cwreagor <cwreagor@gmail.com>
2025-09-17 15:18:07 -0500

    commit

---
+++ Module1/LabExercises/exercise3.js
@@ -0,0 +1,10 @@
+function changeMe() {
+  document.getElementById("column1").style.background = "powderblue";
+  document.getElementById("heading1").innerText = "You";
+  document.getElementById("p1").innerText = "!!!";
+}
+function changeMe2() {
+  document.getElementById("column2").style.background = "lightgreen";
+  document.getElementById("heading2").innerText = "did it!";
+  document.getElementById("p2").innerText = "good job!!! (yes that's my cat)";
+}
