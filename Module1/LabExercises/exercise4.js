dfa2885277db9fdf4bc853ee493847ef336342aa
cwreagor <cwreagor@gmail.com>
2025-09-18 14:12:36 -0500

    commit

---
+++ Module1/LabExercises/exercise4.js
@@ -0,0 +1,64 @@
+function sum(a, b) {
+   console.log(a + b)
+}
+
+sum(10, 15)
+
+function sum (a, b) {
+    return a + b
+}
+
+console.log(sum(10, 15))
+
+
+function difference(a, b) {
+   console.log(a - b)
+}
+
+sum(250, 200)
+
+function difference (a, b) {
+    return a - b
+}
+
+console.log(difference(250, 200))
+
+
+function quotient(a, b) {
+   console.log(a / b)
+}
+
+quotient(375, 5)
+
+function quotient (a, b) {
+    return a / b
+}
+
+console.log(quotient(375, 5))
+
+
+function product(a, b) {
+   console.log(a * b)
+}
+
+product(25, 4)
+
+function product (a, b) {
+    return a * b
+}
+
+console.log(product(25, 4))
+
+
+function greeting(name) {
+    let greeting = "Hello " + name;
+    console.log(greeting);
+}
+
+greeting("Chase")
+
+function greeting(name) {
+    return "Hello " + name;
+}
+
+console.log(greeting("Chase"))
